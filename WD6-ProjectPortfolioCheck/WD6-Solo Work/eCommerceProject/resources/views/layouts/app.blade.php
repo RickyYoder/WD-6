11d658eb838a3c7889ed5308f66b6b667fb1d642
Rick Yoder <RickyYoder@users.noreply.github.com>
2017-11-02 13:56:04 -0400

    Working on Splashpage Navigation

--- WD6-ProjectPortfolioCheck/WD6-Solo Work/eCommerceProject/resources/views/layouts/app.blade.php
+++ WD6-ProjectPortfolioCheck/WD6-Solo Work/eCommerceProject/resources/views/layouts/app.blade.php
@@ -5,12 +5,43 @@
 		<meta name="viewport" content="width=device-width"/>
 		<link rel="stylesheet" href="{{ asset('assets/css/materialize.min.css') }}" />
 		<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+		<script src="https://use.fontawesome.com/2e58db1670.js"></script>
         <title>@yield('title')</title>
     </head>
     <body>
+		<header>
+			<div class="navbar-fixed">
+				<nav>
+					<div class="nav-wrapper">
+						<a class="brand-logo left" href="#!">WD6 Store</a>
+						
+						<ul class="right hide-on-small-only">
+							<li><a href="#!"><i class="fa fa-user fa-lg"></i> My Account</a></li>
+							<li><a href="#!"><i class="fa fa-shopping-cart fa-lg"></i> My Cart</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
+		
 		<main>
 			@yield('content')
 		</main>
+		
+		<footer class="page-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col s12 m4">
+						
+					</div>
+				</div>
+				<div class="row">
+					<div class="col s12 center-align">
+						&copy;2017 Full Sail University
+					</div>
+				</div>
+			</div>
+		</footer>
 		
 		<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 		<script src="{{ asset('assets/js/materialize.min.js') }}"></script>
