60673761c1baaecc23a141b64c5ae54d3148b9df
Rick Yoder <RickyYoder@users.noreply.github.com>
2017-11-02 00:36:58 -0400

    Moving All Front-End Assets to the "public" folder

--- WD6-ProjectPortfolioCheck/WD6-Solo Work/eCommerceProject/resources/views/layouts/app.blade.php
+++ WD6-ProjectPortfolioCheck/WD6-Solo Work/eCommerceProject/resources/views/layouts/app.blade.php
@@ -3,10 +3,8 @@
     <head>
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width"/>
-		<!--
-		<link rel="stylesheet" href="/css/materialize.min.css" />
-		<link rel="stylesheet" href="/css/main.css" />
-		-->
+		<link rel="stylesheet" href="{{ asset('assets/css/materialize.min.css') }}" />
+		<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
         <title>@yield('title')</title>
     </head>
     <body>
@@ -14,10 +12,8 @@
 			@yield('content')
 		</main>
 		
-		<!--
-		<script src="/js/jquery.min.js"></script>
-		<script src="/js/materialize.min.js"></script>
-		<script src="/js/main.js"></script>
-		-->
+		<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+		<script src="{{ asset('assets/js/materialize.min.js') }}"></script>
+		<script src="{{ asset('assets/js/main.js') }}"></script>
     </body>
 </html>
