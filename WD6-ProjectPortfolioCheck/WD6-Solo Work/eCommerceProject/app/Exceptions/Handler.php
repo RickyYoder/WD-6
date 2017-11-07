26a8f4fdfc8f9ee53906e9c83a7f25f69815d9c4
Rick Yoder <RickyYoder@users.noreply.github.com>
2017-11-06 20:39:36 -0500

    Handling of Unauthenticated Users

--- WD6-ProjectPortfolioCheck/WD6-Solo Work/eCommerceProject/app/Exceptions/Handler.php
+++ WD6-ProjectPortfolioCheck/WD6-Solo Work/eCommerceProject/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,6 +49,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+		
+		if ($exception instanceof AuthenticationException) {
+
+            // here goes your code to handle the auth exception, e.g.:
+            return redirect('/signin');
+
+        }
+		
         return parent::render($request, $exception);
     }
 }
